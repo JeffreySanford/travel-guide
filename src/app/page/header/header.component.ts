7dd876307f601aec503eea266c8d8f7d9d3e88d0
Jeffrey Sanford <jeffreysanford@gmail.com>
2018-01-25 07:13:42 -0700

    styling landing page

--- src/app/page/header/header.component.ts
+++ src/app/page/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+const person = {};
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -10,6 +12,7 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
