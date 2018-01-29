a5a561135beb6d7967d35a1f48348d86bdeb437c
Jeffrey Sanford <jeffreysanford@gmail.com>
2018-01-28 17:27:55 -0700

    add in animation elements

--- src/app/page/landing/landing.component.ts
+++ src/app/page/landing/landing.component.ts
@@ -5,7 +5,24 @@ import { trigger, state, style, transition, animate, keyframes } from '@angular/
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.css']
+  styleUrls: ['./landing.component.css'],
+  animations: [
+    trigger('slideInLeft', [
+      transition('void => *', [
+        style({
+          opacity: 0,
+          transform: 'translate3d(-100%, 0, 0)'
+        }),
+        animate('1s 200ms ease-in')
+      ]),
+      transition('* => void', [
+        animate('1s 200ms ease-out', style({
+          opacity: 0,
+          transform: 'translate3d(0, 0, 0)'
+        }))
+      ])
+    ])
+  ]
 })
 
 export class LandingComponent implements OnInit {
--- src/app/page/register/register.component.ts
+++ src/app/page/register/register.component.ts
@@ -1,11 +1,30 @@
 import { Component, OnInit, Output, Input } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
+
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
+  animations: [
+    trigger('slideInLeft', [
+      transition('void => *', [
+        style({
+          opacity: 0,
+          transform: 'translate3d(-100%, 0, 0)'
+        }),
+        animate('1s 200ms ease-in')
+      ]),
+      transition('* => void', [
+        animate('1s 200ms ease-out', style({
+          opacity: 0,
+          transform: 'translate3d(0, 0, 0)'
+        }))
+      ])
+    ])
+  ]
 })
 
 
